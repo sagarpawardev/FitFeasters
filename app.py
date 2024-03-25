6ead999d49e87ac069682dd4167f0c4e2159f94b
Courtney Daly <courtneydaly1@gmail.com>
2024-03-25 18:47:53 -0400

    adding login functionality

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, request, render_template,  redirect, flash, session, requests
+from flask import Flask, request, render_template,  redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db,  connect_db, user
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from werkzeug.exceptions import Unauthorized
+from forms import RegisterForm, LoginForm, DeleteForm
+from models import db,  connect_db, User
 
 
 app = Flask(__name__)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///fitfeasters_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -12,35 +17,140 @@ app.config['SECRET_KEY'] = "food4thesoul"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 debug = DebugToolbarExtension(app)
 
-connect_db(app)
+bcrypt= Bcrypt()
+db = SQLAlchemy()
+db.init_app(app)
 
 
+
+    
 app.app_context().push()
 db.create_all()
-
-
-API_KEY = 'Y43cb4f5704da4cfe9d4d261fbb40c746'
-endpoint = 'https://api.spoonacular.com/recipes/random'
-
-params = {
-    'number': 10,  # Number of random recipes to fetch
-    'apiKey': API_KEY
-}
-
-# Make GET request to Spoonacular API
-response = requests.get(endpoint, params=params)
-
-# Check if request was successful (status code 200)
-if response.status_code == 200:
-    # Extract data from response (assuming JSON format)
-    data = response.json()
     
-    # Process and store data in your database
-    for recipe in data['recipes']:
-        # Example: Store recipe information in your database
-        recipe_id = recipe['id']
-        recipe_title = recipe['title']
-        # Store other relevant recipe information...
-        print(f"Recipe ID: {recipe_id}, Title: {recipe_title}")
-else:
-    print(f"Failed to fetch data: {response.status_code}")
+
+
+
+
+
+
+
+
+# API_KEY = 'Y43cb4f5704da4cfe9d4d261fbb40c746'
+# endpoint = 'https://api.spoonacular.com/recipes/findByIngredients/'
+
+# params = {
+#     'number': 5,  # Number of recipes to fetch
+#     'apiKey': API_KEY
+# }
+
+# # Make GET request to Spoonacular API
+# response = request.get(endpoint, params=params)
+
+# # Check if request was successful (status code 200)
+# if response.status_code == 200:
+#     # Extract data from response 
+#     data = response.json()
+    
+#     # Process and store data in your database
+#     for recipe in data['recipes']:
+#         # Example: Store recipe information in your database
+#         recipe_id = recipe['id']
+#         recipe_title = recipe['title']
+#         # Store other relevant recipe information...
+#         print(f"Recipe ID: {recipe_id}, Title: {recipe_title}")
+# else:
+#     print(f"Failed to fetch data: {response.status_code}")
+
+
+@app.route("/")
+def homepage():
+    """Homepage of site; redirect to register."""
+
+    return redirect("/register")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register a user: produce form and handle form submission."""
+
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+
+        user = User.register(username, password, first_name, last_name, email)
+
+        db.session.commit()
+        session['username'] = user.username
+
+        return redirect(f"/users/{user.username}")
+
+    else:
+        return render_template("/register.html", form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Produce login form or handle login."""
+
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)  # <User> or False
+        if user:
+            session['username'] = user.username
+            return redirect(f"/users/{user.username}")
+        else:
+            form.username.errors = ["Invalid username/password."]
+            return render_template("users/login.html", form=form)
+
+    return render_template("users/login.html", form=form)
+
+
+@app.route("/logout")
+def logout():
+    """Logout route."""
+
+    session.pop("username")
+    return redirect("/login")
+
+
+@app.route("/users/<username>")
+def show_user(username):
+    """Example page for logged-in-users."""
+
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+
+    user = User.query.get(username)
+    form = DeleteForm()
+
+    return render_template("users/show.html", user=user, form=form)
+
+
+@app.route("/users/<username>/delete", methods=["POST"])
+def remove_user(username):
+    """Remove user nad redirect to login."""
+
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+
+    user = User.query.get(username)
+    db.session.delete(user)
+    db.session.commit()
+    session.pop("username")
+
+    return redirect("/login")
---
+++ forms.py
@@ -0,0 +1,50 @@
+"""Forms for fitfeasters app."""
+
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, NumberRange, Email, Optional
+from flask_wtf import FlaskForm
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=20)],
+    )
+
+
+class RegisterForm(FlaskForm):
+    """User registration form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=6, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=20)],
+    )
+    email = StringField(
+        "Email",
+        validators=[InputRequired(), Email(), Length(min= 5, max=25)],
+    )
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+    last_name = StringField(
+        "Last Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+
+
+class DeleteForm(FlaskForm):
+    """Delete form -- this form is intentionally blank."""
+    
+    
